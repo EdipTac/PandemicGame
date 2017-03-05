98cb012f97d319c813f1d99a16d3ce7726ce5632
Wishe Deom <wishedeom@gmail.com>
2017-03-05 15:40:12 -0500

    Comments.

--- Pandemic/TextualCard.h
+++ Pandemic/TextualCard.h
@@ -2,15 +2,23 @@
 
 #include "Card.h"
 
+// Represents a card with a custom name and description
 class TextualCard
 	: virtual public Card
 {
 public:
+	// Constructs a card with a given name and description
 	TextualCard(std::string name, std::string description);
+
+	// Virutal destructor to make class abstract
 	virtual ~TextualCard() = 0;
+
+	// The card's name
 	virtual std::string name() const override;
-	virtual std::string description() const override;
 	void setName(const std::string& name);
+
+	// The card's description
+	virtual std::string description() const override;
 	void setDescription(const std::string& description);
 
 private:
