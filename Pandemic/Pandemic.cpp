c39a044c6e229edf29e179cda867e5efe2d90650
Wishe Deom <wishedeom@gmail.com>
2017-03-05 15:59:31 -0500

    Brought in UI file-loading functions.

--- Pandemic/Pandemic.cpp
+++ Pandemic/Pandemic.cpp
@@ -13,9 +13,9 @@
 //
 // An implementation of the board game "Pandemic" by Z-Man Games.
 
-#pragma warning (disable: 4592)
+#pragma warning (disable: 4592)	// Static initialization warning
 
-//  ----    Inclusions    ----
+//  ----    Inclusions    ----  //
 // Standard library inclusions
 #include <iostream>
 #include <fstream>
@@ -35,11 +35,15 @@
 #include "Util.h"
 #include "InfectionCard.h"
 #include "InfectionCardDeck.h"
-//#include "Serialization.h"
+#include "Serialization.h"
 
 void newGame();
 void loadGame();
 void waitForExit();
+std::string solicitFileName();
+Map loadMapFile();
+City& solicitCity(const Map& map);
+std::string solicitPlayerName(const size_t number);
 
 static const Menu mainMenu
 {
@@ -50,7 +54,7 @@ static const Menu mainMenu
 	}
 };
 
-//	----    Program entry point    ----
+//	----    Program entry point    ----  //
 void main()
 {
 	readMapFromFile("earth.map");
@@ -62,6 +66,7 @@ void main()
 void newGame()
 {
 	std::cout << "New game...\n";
+	solicitFileName();
 }
 
 void loadGame()
@@ -73,4 +78,62 @@ void waitForExit()
 {
 	std::cout << "Press any key to continue...\n";
 	std::cin.get();
+}
+
+std::string solicitFileName()
+{
+	std::string fileName;
+	while (true)
+	{
+		std::cout << "Load map file to start new game: ";
+		std::cin >> fileName;
+		if (fileExists(fileName))
+		{
+			break;
+		}
+		std::cout << "File not found.\n";
+	}
+	return fileName;
+}
+
+Map loadMapFile()
+{
+	const auto fileName = solicitFileName();
+	std::cout << "\nLoading map \"" << fileName << "\"...\n";
+	auto map = readMapFromFile(fileName);
+	std::cout << "Map \"" << fileName << "\" loaded!\n\n";
+	return map;
+}
+
+City& solicitCity(const Map& map)
+{
+	std::cout << "Where would you like to place your pawn? ";
+	std::string cityName;
+	while (true)
+	{
+		std::cin >> cityName;
+		if (map.contains(cityName))
+		{
+			break;
+		}
+		std::cout << "No city of that name exists.\n";
+	}
+	return map.city(cityName);
+}
+
+std::string solicitPlayerName(const size_t number)
+{
+	std::cout << "Enter the name of player " << number << ": ";
+	std::string playerName;
+	while (true)
+	{
+		std::getline(std::cin >> std::ws, playerName);
+		if (!playerName.empty())
+		{
+			break;
+		}
+		std::cout << "Player name cannot be empty.\n";
+	}
+	std::cout << "Welcome, " << playerName << "\n\n";
+	return playerName;
 }
