46264ae067be9e2106e26c7dc50fcc911503d563
Wishe Deom <wishedeom@gmail.com>
2017-03-29 19:02:53 -0400

    impl Action

---
+++ Pandemic/Action.h
@@ -0,0 +1,18 @@
+// Represents a single action that a player can take
+// Default actions that all players can take are...
+
+#pragma once
+
+#include <string>
+
+class Board;
+
+namespace action
+{
+	class Action
+	{
+	public:
+		//std::string name() const;
+		//bool available(Board& board);
+	};
+}
