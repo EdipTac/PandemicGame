4026c70d9f08e7e2f36569512b5a32c88816cf6a
Wishe Deom <wishedeom@gmail.com>
2017-04-01 12:32:16 -0400

    impl TakeKnowledge

--- Pandemic/TakeKnowledge.cpp
+++ Pandemic/TakeKnowledge.cpp
@@ -1,47 +1,39 @@
-#include <stdexcept>
-
-#include "GiveKnowledge.h"
+#include "TakeKnowledge.h"
 #include "Player.h"
 #include "City.h"
 #include "Board.h"
 
 const std::string desc =
-	"GIVE the city card that matches the city you are in to another player. The other player "
+	"TAKE a city card that matches the city you are in from another player. The other player "
 	"must also be in the city with you. Both of you need to agree to do this. If the player who gets the card "
 	"now has more than 7 cards, that player must immediately discard a card or play an Event card.";
 
-action::GiveKnowledge::GiveKnowledge(Player* const performer)
-	: Action { "Give Knowledge", desc, performer }
+action::TakeKnowledge::TakeKnowledge(Player* const performer)
+	: Action { "Take Knowledge", desc, performer }
 {}
 
-void action::GiveKnowledge::solicitData()
+void action::TakeKnowledge::solicitData()
 {
 	// Aliases
 	const auto& position = _performer->pawn().position();
 	const auto& players = Board::instance().players();
 
-	// Do you have a city card matching your position to give?
-	_card = _performer->positionCard();
-	if (!_card)
-	{
-		std::cout << "You have no city card matching your position.\n";
-		return;
-	}
-
-	// If so, select another player in your city
+	// Get other people in the city with a matching city card
 	std::vector<Player*> others;
 	for (const auto& player : players)
 	{
-		if (player != _performer && player->pawn().position() == position)
+		if (player != _performer
+			&& player->pawn().position() == position
+			&& player->positionCard())
 		{
 			others.push_back(player);
 		}
 	}
 
-	// If there are none, you cannot give
+	// If there are none, you cannot take
 	if (others.empty())
 	{
-		std::cout << "No other players in your city.\n";
+		std::cout << "No other players with a matching city card in your city.\n";
 		return;
 	}
 
@@ -66,14 +58,17 @@ void action::GiveKnowledge::solicitData()
 		}
 		std::cout << "Not a player in this city.\n";
 	}
+
+	// Prepare their city card for transfer
+	_card = _target->positionCard();
 }
 
-void action::GiveKnowledge::perform()
+void action::TakeKnowledge::perform()
 {
-	_performer->giveCard(*_card, *_target);
+	_target->giveCard(*_card, *_performer);
 }
 
-bool action::GiveKnowledge::isValid() const
+bool action::TakeKnowledge::isValid() const
 {
 	return _performer && _card && _target;
 }
