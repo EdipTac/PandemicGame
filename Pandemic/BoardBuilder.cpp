7ca55ecdacf88206efcc57e26fc9e45b304bf075
Jonny <linton.jonny@gmail.com>
2017-04-16 12:11:42 -0400

    Fixing issue #19

--- Pandemic/BoardBuilder.cpp
+++ Pandemic/BoardBuilder.cpp
@@ -115,7 +115,7 @@ BoardBuilder& BoardBuilder::loadPlayers()
 		for (auto it = cities.begin(); it != cities.end(); ++it) {
 			if ((*it)->name() == cityName) {
 				// found a match
-				player->moveTo(**it);
+				player->pawn().setPosition(**it);
 			}
 		}
 
